9beb944c4badd7ec80e318703f6f0d18323f8cb5
kong-wep <pakkapol.20100@gmail.com>
2022-12-09 22:18:44 +0700

    Modified left margin

--- frontend/src/components/About/index.tsx
+++ frontend/src/components/About/index.tsx
@@ -5,14 +5,14 @@ export default function About() {
     <div>
       <h1 className="text-7xl font-bold mt-32 ml-24">About Us</h1>
       {/* Right Div */}
-      <div className="bg-Blond20 absolute z-20 top-[20rem] left-[45%] py-14 px-12 max-w-[550px] rounded-3xl drop-shadow-[2px_4px_48px_rgba(0,0,0,0.1)]">
+      <div className="bg-Blond20 absolute z-20 top-[20rem] left-[40%] py-14 px-12 max-w-[550px] rounded-3xl drop-shadow-[2px_4px_48px_rgba(0,0,0,0.1)]">
         <h2 className="text-5xl font-bold mb-6">
           What is <span className='text-Rusty'>Barcamp</span> ?
         </h2>
         <p className="font-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
       {/* Left Div */}
-      <div className="bg-Blond20 z-10 absolute top-[40rem] left-[20%] py-14 px-12 max-w-[550px] rounded-3xl drop-shadow-[2px_4px_48px_rgba(0,0,0,0.1)]">
+      <div className="bg-Blond20 z-10 absolute top-[40rem] left-[15%] py-14 px-12 max-w-[550px] rounded-3xl drop-shadow-[2px_4px_48px_rgba(0,0,0,0.1)]">
         <h2 className="text-5xl font-bold mb-6">Participants</h2>
         <p className="font-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
