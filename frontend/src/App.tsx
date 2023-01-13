95f0d340ba71a8abe1ad485dbf54360e4eccdc59
tboonma <tboonma@hotmail.com>
2023-01-13 15:30:32 +0700

    Changed to in progree bcoz no information yet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import Session from "./components/Session";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import MobileSidebar from "./components/Sidebar";
+import InProgress from "./components/InProgress";
 
 function App():JSX.Element {
   return (
@@ -20,8 +21,8 @@ function App():JSX.Element {
         <Route path="/" element={<Home/>} />
         <Route path="about" element={<About/>} />
         <Route path="faqs" element={<FAQS/>} />
-        <Route path="timetable" element={<TimeTable/>} />
-        <Route path="session" element={<Session/>} />
+        <Route path="timetable" element={<InProgress />} />
+        <Route path="session" element={<InProgress />} />
       </Routes>
       <Footer/>
     </React.Fragment>
---
+++ frontend/src/components/InProgress/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'flowbite'
+
+export default function Session() {
+  return (
+    <div className="container max-w-7xl mx-auto mt-4 md:mt-16">
+      <div className="p-32 text-center">
+        <h3 className="text-5xl text-Falu100">Coming Soon !</h3>
+      </div>
+    </div>
+  )
+}
