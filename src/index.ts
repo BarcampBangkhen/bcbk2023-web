cbd59c7508258e57d30dd60385858ffd2ff8e778
tboonma <tboonma@hotmail.com>
2023-01-14 13:07:45 +0700

    Add cors config

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,17 @@ import faqRouter from './routes/faq.route'
 import { connect, set } from 'mongoose'
 import { Constant } from './constant'
 import path from 'path'
-import fs from 'fs'
+import cors from 'cors'
 
 const app = express()
 const port = Constant.Port
 const staticPath = path.normalize(
   path.join(__dirname, '..', 'frontend', 'build')
 )
+const corsOptions = {
+  origin: 'localhost',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 app.use(express.json())
 app.use(express.static(staticPath))
@@ -17,17 +21,7 @@ app.use(express.static(staticPath))
 const apiRouter = Router()
 apiRouter.use('/faq', faqRouter)
 
-app.use('/api', apiRouter)
-
-app.get('/debug', async (req: Request, res: Response) => {
-  let selectedPath = path.join(__dirname, '..', 'frontend')
-   fs.readdir(selectedPath, async (err, files) => {
-    files.forEach((file) => {
-      console.log(file)
-    })
-  })
-  res.status(200).json({ Data: selectedPath})
-})
+app.use('/api', cors(corsOptions), apiRouter)
 
 app.get('*', (req: Request, res: Response) => {
   res.sendFile(path.join(staticPath, 'index.html'))
