a844c6acd036a588b4274c857e97cc9d3a5913b1
tboonma <tboonma@hotmail.com>
2023-01-22 17:02:37 +0700

    Try to hide api route

--- src/routes/faq.route.ts
+++ src/routes/faq.route.ts
@@ -1,16 +1,17 @@
-import { Router } from 'express'
+import { Request, Response, Router } from 'express'
 import {
   GetAllAnsweredQuestions,
   GetAllQuestions,
   PatchQuestion,
   PutQuestion
 } from '../controllers/faq.controller'
+import { preventManualAccess } from '../utils/ApiMiddleware'
 
 const router = Router()
 
-router.get('/', GetAllAnsweredQuestions)
+router.get('/', preventManualAccess, GetAllAnsweredQuestions)
 
-router.put('/', PutQuestion)
+router.put('/', preventManualAccess, PutQuestion)
 
 router.get('/manage', GetAllQuestions)
 
